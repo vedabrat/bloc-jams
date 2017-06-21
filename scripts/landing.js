61ce3ed28117d733a54f2921ad5c31cb93a34e8a
vedabrat <vedabrat>
2017-06-21 12:25:23 -0400

    very good

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,28 +4,22 @@ var revealPoint = function(point) {
     point.style.msTransform = "scaleX(1) translateY(0)";
     point.style.WebkitTransform = "scaleX(1) translateY(0)";
 }
-
 var points = document.getElementsByClassName('point');
-
 var animatePoints = function(points) {
     for (var i = 0; i < points.length; i++) {
         revealPoint(points[i]);
     }
 };
-
 window.onload = function() {
-         if (window.innerHeight > 950) {
-         animatePoints(points);
-     }
-
+    if (window.innerHeight > 950) {
+        animatePoints(points);
+    }
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
     window.addEventListener('scroll', function(event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(points);   
-         }
-        
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(points);
+        }
     });
     animatePoints(points);
 };
