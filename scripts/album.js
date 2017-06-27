4f9387366ed6a5b3e9c940f3a99daf1687ee9ae8
vedabrat <vedabrat>
2017-06-27 15:15:46 -0400

    checkpoint15 done

--- scripts/album.js
+++ scripts/album.js
@@ -62,19 +62,24 @@ var songListContainer = document.getElementsByClassName('album-view-song-list')[
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
- var currentlyPlayingSong = null;
-  var clickHandler = function(targetElement) {   
-        var songItem = getSongItem(targetElement);  
+  // Store state of playing songs
+var currentlyPlayingSong = null;
+var clickHandler = function(targetElement) {   
+    var songItem = getSongItem(targetElement);  
      if (currentlyPlayingSong === null) {
          songItem.innerHTML = pauseButtonTemplate;
          currentlyPlayingSong = songItem.getAttribute('data-song-number');
-          } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
          songItem.innerHTML = playButtonTemplate;
          currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+
      }
     };
-
-
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
@@ -108,17 +113,27 @@ window.onload = function() {
 };
 
 var findParentByClassName = function(element, targetClass) {
+    // console.log("No parent found")
+    // console.log("No parent found with that class name")
+    
+ /*   if (element.findParentByClassName.split(' ').indexOf(classname)>=0) return true;
+    return element.parentNode && hasSomeParentTheClass(element.parentNode, classname);
+}*/    
     if (element) {
         var currentParent = element.parentElement;
+
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
+        console.log(currentParent);
+
         return currentParent;
     }
 };
 
 
 var getSongItem = function(element) {
+
     switch (element.className) {
         case 'album-song-button':
         case 'ion-play':
@@ -135,4 +150,3 @@ var getSongItem = function(element) {
             return;
     }  
 };
-
